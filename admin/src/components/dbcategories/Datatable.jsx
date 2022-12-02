b60cc9edaa9c049ad7aaa48fd9fe1059bd06750a
Duc Nguyen <mingduc17082002@gmail.com>
2022-12-02 15:42:39 +0700

    Change text in category

--- admin/src/components/dbcategories/Datatable.jsx
+++ admin/src/components/dbcategories/Datatable.jsx
@@ -65,7 +65,7 @@ const Datatable = () => {
   return (
     <div className="datatable">
       <div className="datatableTitle">
-        Products
+        Categories
         <Link to="/dashboard/categories/new" className="link">
           Add New
         </Link>
